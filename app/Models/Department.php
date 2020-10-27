f798256dd974464180e188d7b02c7a79cffd7dd5
doublebean <229763607@qq.com>
2020-10-27 16:54:36 +0800

    '唐邦彦第一次提交'

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
@@ -9,4 +10,107 @@ class Department extends Model
     protected $table = "department";
     public $timestamps = true;
     protected $guarded = [];
+
+    /**
+     * 展示系部页面
+     * @author tangbangyan <github.com/doublebean>
+     * @return mixed
+     */
+    public static function tby_showDepartment()
+    {
+        try{
+            $date=department::select('*')
+                ->paginate(5);
+              return $date;
+        }catch (Exception $e){
+            logger::Error('系部界面展示失败',[$e->getMessage()]);
+        }
+    }
+
+    /**
+     * 回显当前数据的系部
+     * @author tangbangyan <github.com/doublebean>
+     * @return mixed
+     */
+    public static function tby_reShowDepartment($department_id)
+    {
+        try{
+            $date=department::where('department_id',$department_id)
+                ->get();
+
+            return $date;
+        }catch (Exception $e){
+            logger::Error('系部界面展示失败',[$e->getMessage()]);
+        }
+    }
+    /**
+     * 删除系部
+     * @author tangbangyan <github.com/doublebean>
+     * @return mixed
+     */
+    public static function tby_deleteDepartment($department_id)
+    {
+        try{
+            $date=department::where('department_id',$department_id)
+                ->delete();
+
+            return $date;
+        }catch (Exception $e){
+            logger::Error('删除失败',[$e->getMessage()]);
+        }
+    }
+
+    /**
+     * 增加系部
+     * @author tangbangyan <github.com/doublebean>
+     * @return mixed
+     */
+    public static function tby_addDepartment($tby)
+    {
+        try{
+
+            $date=department::create([
+                'department_name'=>$tby['department_name']
+            ]);
+
+            return true;
+        }catch (Exception $e){
+            logger::Error('增加失败',[$e->getMessage()]);
+        }
+    }
+
+    /**
+     * 修改系部
+     * @author tangbangyan <github.com/doublebean>
+     * @return mixed
+     */
+    public static function tby_modifyDepartment($tby)
+    {
+        try{
+            $date=department::where('department_id',$tby['department_id'])
+                ->update([
+                'department_name'=>$tby['department_name']
+            ]);
+            return true;
+        }catch (Exception $e){
+            logger::Error('修改失败',[$e->getMessage()]);
+        }
+    }
+
+    /**
+     * 查询系部
+     * @author tangbangyan <github.com/doublebean>
+     * @return mixed
+     */
+    public static function tby_findDepartment($tby)
+    {
+        try{
+            $date=department::where('department_name',$tby['department_name'])
+                ->paginate(5);
+            return $date;
+        }catch (Exception $e){
+            logger::Error('系部查询界面展示失败',[$e->getMessage()]);
+        }
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,20 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 Route::get('test','TestController@test');
+Route::prefix('supadmin')->namespace('SupAdmin')->group(function (){
+    Route::get('showdepartment','ClassController@showdepartment');//系部管理页面展示
+    Route::get('reshowdepartment','ClassController@reshowdepartment');//回显当前数据的系部
+    Route::post('deletedepartment','ClassController@deletedepartment');//删除系部
+    Route::post('adddepartment','ClassController@adddepartment');//增加系部
+    Route::post('modifydepartment','ClassController@modifydepartment');//修改系部
+    Route::get('finddepartment','ClassController@finddepartment');//查询系部
+
+    Route::get('showclass','ClassController@showclass');//班级管理页面展示
+    Route::get('reshowclass','ClassController@reshowclass');//回显当前数据的班级和系部
+    Route::post('deleteclass','ClassController@deleteclass');//删除班级
+    Route::post('addclass','ClassController@addclass');//增加班级
+    Route::post('modifyclass','ClassController@modifyclass');//修改班级
+    Route::get('findclass','ClassController@findclass');//查询班级
+});
+
