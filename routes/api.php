fe9334d72c5c3e81bc47400bb790fbd34820e1d9
YetiSui <3209948743@qq.com>
2020-10-29 12:59:02 +0800

    张巾巾的第一次提交

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * @author  ZhangJinJIn <github.com/YetiSui>
+ */
+Route::prefix('supadmin')->namespace("SupAdmin")->group(function () {
+    Route::post('adddevice', 'EquipmentController@addDevice'); //新增设备
+    Route::get('searchnew', 'EquipmentController@searchNew'); //查询设备
+    Route::get('goback', 'EquipmentController@goBack'); //回显
+    Route::post('exitnew', 'EquipmentController@exitNew'); //修改设备信息
+    Route::post('rmnew', 'EquipmentController@rmNew'); //删除设备信息
+    Route::get('shownew', 'EquipmentController@showNew'); //设备管理页面展示
+    });
